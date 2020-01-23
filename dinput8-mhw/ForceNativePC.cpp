fb6faad9f36d83e3a6cf4f014e84c7fda9a490cc
Strackeror <rremilavergne@gmail.com>
2020-01-23 16:13:17 +0100

    New nativePC enforcing method

--- dinput8-mhw/ForceNativePC.cpp
+++ dinput8-mhw/ForceNativePC.cpp
@@ -11,41 +11,29 @@
 // 48 89 e0 41 55 41 57 48 81 ec 68 01 00 00
 #define FilePathCheckOffset	0x1609bc430
 
+// 74 44 45 31 c0 48 8d 93 f8 62 21 00 48 8b 02 48 85 c0 74 23 48 8b 4d 08 48 39 48 08 74 28
+#define NativePCCheckOffset
+
 std::map<std::string, std::string> nativePCList;
 bool outputEveryPath = false;
 
 HOOKFUNC(FilePathCheck, void*, void* _0, const char* path, void* _1)
 {
 	std::string spath(path);
-	if (outputEveryPath)
-		LOG(INFO) << "File loaded : " << spath;
-	std::transform(spath.begin(), spath.end(), spath.begin(), std::tolower);
-	if (nativePCList.find(spath) == nativePCList.end())
-		return originalFilePathCheck(_0, path, _1);
-	LOG(INFO) << "File redirected to nativePC : " << spath;
-	return originalFilePathCheck(_0, nativePCList[spath].c_str(), _1);
-}
-
-void FetchNativePCFiles()
-{
-	if (!std::filesystem::is_directory("nativePC")) return;
-	for (auto entry : std::filesystem::recursive_directory_iterator("nativePC"))
-	{
-		if (!entry.is_regular_file()) continue;
-		std::string path = entry.path().string();
-		path.erase(0, 9);
-		std::transform(path.begin(), path.end(), path.begin(), std::tolower);
-		nativePCList["native:\\" + path] = "nativePC:\\" + path;
-	}
+	LOG(INFO) << "File loaded : " << spath;
+	return originalFilePathCheck(_0, path, _1);
 }
 
 void InjectForceNativePC()
 {
 	outputEveryPath = ConfigFile.value<bool>("outputEveryPath", false);
-	FetchNativePCFiles();
-	if (!nativePCList.empty() || outputEveryPath)
+
+	LOG(INFO) << "Hooking Force NativePC";
+	unsigned char buf[] = { 0xEB };
+	memcpy((void*)0x160ae9498, buf, 1);
+
+	if (outputEveryPath)
 	{
-		LOG(INFO) << "Hooking Force NativePC";
 		AddHook(FilePathCheck, FilePathCheckOffset);
 	}
 }
