d22a4e9e17ce1231aa212a62039f21d486ba7203
Strackeror <rremilavergne@gmail.com>
2020-02-09 19:47:17 +0100

    Multiplayer Tenderize override, disable override when in invalid situation

--- dinput8-mhw/QOL.cpp
+++ dinput8-mhw/QOL.cpp
@@ -1,3 +1,4 @@
+#include <set>
 
 #include "dll.h"
 #include "log.h"
@@ -6,13 +7,15 @@
 // 48 89 5c 24 08 48 89 6c 24 10 48 89 74 24 18 57 48 83 ec 20 48 89 d6 48 89 cf 48 85 d2 0f 84 43 01 00 00 48 8b 49 08
 #define	TenderizePartAddress	0x14df814d0
 
+//48 81 c1 f0 c3 01 00 45 31 c0
+#define TenderizePartMPAddress  0x15dce4170
+
 // 40 53 48 83 ec 20 48 8b 81 70 76 00 00 0f 57 c0 48 89 cb 0f 2f 40 64 73 59 8b 81 20 ea 01 00
 #define TurnClawCheckAddress	0x15dcc6740
 
 // 84 c0 74 1e 48 8b 8b e0 89 00 00 48 8d 54 24 38 41 b8 01 00 00 00 48 8b 89 30 01 00 00
 // Substract 0x18
 #define LaunchActionAddress		0x15dce8110
-
 
 template<typename T>
 inline T* offsetPtr(void* ptr, int offset) { return (T*)(((char*)ptr) + offset); }
@@ -54,7 +57,7 @@ char* getLastActionName(void* monster)
 }
 
 
-static bool actionUsed = false;
+static std::set<void*> actionUsed;
 HOOKFUNC(TurnClawCheck, bool, void* monster)
 {
 	std::string action(getLastActionName(monster));
@@ -66,35 +69,46 @@ HOOKFUNC(TurnClawCheck, bool, void* monster)
 // rage status -> monster+0x1bddc
 // rage count -> monster+0x1bdfc
 
-HOOKFUNC(TenderizePart, bool, void* obj, void* data, float dmg)
+bool CheckTenderize(void* monster, float dmg)
 {
-	void* monster = *(void**) offsetPtr(obj, 8);
-
-	float* duration = (float*) offsetPtr(monster, 0x1c3e0 + 0x4a0);
+	float* duration = (float*) offsetPtr(monster, 0x1c3f0 + 0x4a0);
 	*duration = 5;
 	
 	char* actionName = getLastActionName(monster);
-
-	LOG(INFO) << "Attempting tenderize : " << actionName << " action already used ? " << actionUsed;
-	LOG(INFO) << "Tenderize value : " << dmg;
-	if ((std::string(actionName).find("_EXTEND") != -1 || std::string(actionName).find("_RIDE_DOWN"))
-		&& !actionUsed
+	LOG(INFO) << "Attempting tenderize : " << actionName;
+	if ((std::string(actionName).find("_EXTEND") != -1 || std::string(actionName).find("_RIDE_DOWN") != -1)
+		&& actionUsed.find(monster) == actionUsed.end()
 		&& (int) dmg != 20) // prevent claw slap tenderizing
 	{
 		LOG(INFO) << "Tenderize succesful !";
-		actionUsed = true;
+		actionUsed.emplace(monster);
 		*duration = 3000;
-		return originalTenderizePart(obj, data, 100);
+		return true;
 	}
+	return false;
+}
 
-	return originalTenderizePart(obj, data, dmg);
+HOOKFUNC(TenderizePart, bool, void* obj, void* data, float dmg)
+{
+	void* monster = *(void**) offsetPtr(obj, 8);
+	bool overrideTenderize = CheckTenderize(monster, dmg);
+	if (overrideTenderize)
+		return originalTenderizePart(obj, data, 100);
+	return true;
+}
+
+HOOKFUNC(TenderizePartMP, void, void* monster, long long* params)
+{
+	if (CheckTenderize(monster, 100))
+		originalTenderizePartMP(monster, params);
 }
 
 HOOKFUNC(LaunchAction, bool, void* monster, int actionId)
 {
-	actionUsed = false;
+	if (actionUsed.find(monster) != actionUsed.end())
+		actionUsed.erase(monster);
 	bool ret = originalLaunchAction(monster, actionId);
-	LOG(INFO) << getLastActionName(monster);
+	//LOG(INFO) << getLastActionName(monster);
 	return ret;
 }
 
@@ -103,4 +117,5 @@ void InjectQOL()
 	AddHook(TenderizePart, TenderizePartAddress);
 	AddHook(TurnClawCheck, TurnClawCheckAddress);
 	AddHook(LaunchAction, LaunchActionAddress);
+	AddHook(TenderizePartMP, TenderizePartMPAddress);
 }
