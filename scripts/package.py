04aad5f49ccf5c7555eceb201ce334d8e185edf1
Strackeror <rremilavergne@gmail.com>
2020-02-23 23:08:22 +0100

    Packaging

--- scripts/package.py
+++ scripts/package.py
@@ -1,6 +1,7 @@
 import os
 from zipfile import ZipFile
 import shutil
+import glob
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -10,6 +11,8 @@ with ZipFile('../x64/Loader-Release.zip', 'w') as zip:
 
 with ZipFile('../x64/Clutch-Release.zip', 'w') as zip:
     zip.write('../x64/Release/ClutchRework.dll', 'nativePC/plugins/ClutchRework.dll')
+    for path in glob.iglob(r'../Plugins/ClutchRework/nativePC/**/*.col', recursive=True):
+        zip.write(path, os.path.relpath(path, r'../Plugins/ClutchRework/'))
 
 shutil.copyfile("../x64/Release/dinput8.lib", "../Plugins/ExamplePlugin/dependencies/dinput8.lib")
 shutil.copyfile("../dinput8-mhw/loader.h", "../Plugins/ExamplePlugin/dependencies/loader.h")
