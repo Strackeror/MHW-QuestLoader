6476c3848448b46538a947c366e0c270065a7a6b
SupaStuff <Stuff_Git_NaoResposta@outlook.com>
2020-12-10 15:11:51 -0500

    omitting Clutch-Release if not built

--- scripts/package.py
+++ scripts/package.py
@@ -12,10 +12,15 @@ with ZipFile('x64/Loader-Release.zip', 'w') as zip:
     zip.write("x64/Release/QuestLoader.dll", "nativePC/plugins/QuestLoader.dll")
     zip.write("x64/Release/MonsterLoader.dll", "nativePC/plugins/MonsterLoader.dll")
 
-with ZipFile('x64/Clutch-Release.zip', 'w') as zip:
-    zip.write('x64/Release/ClutchRework.dll', 'nativePC/plugins/ClutchRework.dll')
-    for path in glob.iglob(r'Plugins/ClutchRework/nativePC/**/*.col', recursive=True):
-        zip.write(path, os.path.relpath(path, r'Plugins/ClutchRework/'))
+try:
+    with ZipFile('x64/Clutch-Release.zip', 'w') as zip:
+        zip.write('x64/Release/ClutchRework.dll', 'nativePC/plugins/ClutchRework.dll')
+        for path in glob.iglob(r'Plugins/ClutchRework/nativePC/**/*.col', recursive=True):
+            zip.write(path, os.path.relpath(path, r'Plugins/ClutchRework/'))
+except FileNotFoundError as e:
+    print("ClutchRework was not built")
+    print(e)
+    os.remove("x64/Clutch-Release.zip")
 
 shutil.copyfile("x64/Release/loader.lib", "Plugins/dependencies/loader.lib")
 shutil.copyfile("MHWLoader/loader.h", "Plugins/dependencies/loader.h")
