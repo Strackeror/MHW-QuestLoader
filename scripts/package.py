61f972e43ba627f0415ce7887f30c2f25f608ce4
Strackeror <rremilavergne@gmail.com>
2023-06-02 13:31:05 +0200

    Update packager

--- scripts/package.py
+++ scripts/package.py
@@ -1,21 +1,15 @@
 import os
 from zipfile import ZipFile
 import shutil
-import glob
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)) + '/..')
 
 with ZipFile('x64/Loader-Release.zip', 'w') as zip:
     zip.write("x64/Release/loader.dll", "loader.dll")
-    zip.write("x64/Release/injector.dll", "hid.dll")
+    zip.write("x64/Release/dinput8.dll", "dinput8.dll")
     zip.write("x64/Release/loader-config.json", "loader-config.json")
     zip.write("x64/Release/QuestLoader.dll", "nativePC/plugins/QuestLoader.dll")
     zip.write("x64/Release/MonsterLoader.dll", "nativePC/plugins/MonsterLoader.dll")
-
-with ZipFile('x64/Clutch-Release.zip', 'w') as zip:
-    zip.write('x64/Release/ClutchRework.dll', 'nativePC/plugins/ClutchRework.dll')
-    for path in glob.iglob(r'Plugins/ClutchRework/nativePC/**/*.col', recursive=True):
-        zip.write(path, os.path.relpath(path, r'Plugins/ClutchRework/'))
 
 shutil.copyfile("x64/Release/loader.lib", "Plugins/dependencies/loader.lib")
 shutil.copyfile("MHWLoader/loader.h", "Plugins/dependencies/loader.h")
